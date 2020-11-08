4ce402df2f2729aaa0867484da3ad04c5ae61969
AbigailAlonso <L17111878@cdjuarez.tecnm.mx>
2020-11-07 21:48:59 -0700

    'grid'

---
+++ js/js/script.js
@@ -0,0 +1,15 @@
+console.log(5);
+console.log("Hola mundo!".length);
+console.log(234.456);
+console.log(2+3);
+console.log(34-12);
+//Tipos de datos: Numericos, String, Boolean, null, undefinded, synbol, Object
+//Variables
+var x=10;
+x= 'Hola';
+let y = 15;
+const vacio = false;
+
+/*
+comentarios
+*/
